da60a31aa8d79d9e9f7eba4170664260be2e1324
Thibault Falise <thibault.falise@gmail.com>
2016-06-27 14:37:30 +0200

    Base game and turn read logic

--- CodeBusters/CodinGameCode.cs
+++ CodeBusters/CodinGameCode.cs
@@ -49,34 +49,23 @@ class Game
     {
         ReadGameParameters();
 
-        // game loop
         while (true)
         {
-            ProcessTurn();
+            var turn = ReadTurn();
+            var instructions = turn.Resolve();
+
+            foreach (var playerBuster in turn.PlayerBusters)
+            {
+                _console.WriteLine(instructions[playerBuster.Id].ToString());
+            }
         }
     }
 
-    public void ProcessTurn()
+    public GameTurn ReadTurn()
     {
-        int entities = int.Parse(_console.ReadLine()); // the number of busters and ghosts visible to you
-        for (int i = 0; i < entities; i++)
-        {
-            string[] inputs = _console.ReadLine().Split(' ');
-            int entityId = int.Parse(inputs[0]); // buster id or ghost id
-            int x = int.Parse(inputs[1]);
-            int y = int.Parse(inputs[2]); // position of this buster / ghost
-            int entityType = int.Parse(inputs[3]); // the team id if it is a buster, -1 if it is a ghost.
-            int state = int.Parse(inputs[4]); // For busters: 0=idle, 1=carrying a ghost.
-            int value = int.Parse(inputs[5]);
-            // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
-        }
-        for (int i = 0; i < _bustersPerPlayer; i++)
-        {
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
-            _console.WriteLine("MOVE 8000 4500");
-        }
+        var gameTurn = new GameTurn(_myTeamId, _bustersPerPlayer, _ghostCount);
+        gameTurn.ReadEntities(_console);
+        return gameTurn;
     }
 
     private void ReadGameParameters()
@@ -85,6 +74,141 @@ class Game
         _ghostCount = int.Parse(_console.ReadLine());
         _myTeamId = int.Parse(_console.ReadLine());
         // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
+    }
+}
+
+internal class GameTurn
+{
+    private int _bustersPerPlayer;
+    private int _ghostCount;
+    private int _teamId;
+
+    private readonly List<Ghost> _ghosts = new List<Ghost>();
+    private readonly List<Buster> _teamBusters = new List<Buster>();
+
+    public GameTurn(int teamId, int bustersPerPlayer, int ghostCount)
+    {
+        _teamId = teamId;
+        _bustersPerPlayer = bustersPerPlayer;
+        _ghostCount = ghostCount;
+    }
+
+    public IEnumerable<Buster> PlayerBusters => _teamBusters;
+
+    public IDictionary<int, IBusterInstruction> Resolve()
+    {
+        var instructions = new Dictionary<int, IBusterInstruction>();
+
+        foreach (var buster in _teamBusters)
+        {
+            instructions.Add(buster.Id, new MoveInstruction(8000, 4500));
+        }
+
+        return instructions;
+    }
+
+    public void ReadEntities(IConsole console)
+    {
+        var entities = int.Parse(console.ReadLine()); // the number of busters and ghosts visible to you
+        for (var i = 0; i < entities; i++)
+        {
+            ReadEntity(console.ReadLine());
+        }
+    }
+
+    private void ReadEntity(string text)
+    {
+        string[] inputs = text.Split(' ');
+        int entityId = int.Parse(inputs[0]); // buster id or ghost id
+        int x = int.Parse(inputs[1]);
+        int y = int.Parse(inputs[2]); // position of this buster / ghost
+        int entityType = int.Parse(inputs[3]); // the team id if it is a buster, -1 if it is a ghost.
+        int state = int.Parse(inputs[4]); // For busters: 0=idle, 1=carrying a ghost.
+        int value = int.Parse(inputs[5]);
+        // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
+
+        if (entityType == -1)
+            _ghosts.Add(new Ghost(entityId, x, y, value));
+        else if (entityType == _teamId)
+            _teamBusters.Add(new Buster(entityId, x, y, state, value));
+    }
+}
+
+internal class Buster
+{
+    public int Id { get; }
+    public int X { get; }
+    public int Y { get; }
+
+    public Buster(int id, int x, int y, int state, int ghostId)
+    {
+        Id = id;
+        X = x;
+        Y = y;
+        IsCarryingGhost = (state == 1);
+        CarriedGhostId = ghostId;
+    }
+
+    public int CarriedGhostId { get; }
+
+    public bool IsCarryingGhost { get; }
+}
+
+internal class Ghost
+{
+    public int Id { get; }
+    public int X { get; }
+    public int Y { get; }
+    public int BustersTrappingGhost { get; }
+
+    public Ghost(int id, int x, int y, int bustersTrappingGhost)
+    {
+        Id = id;
+        X = x;
+        Y = y;
+        BustersTrappingGhost = bustersTrappingGhost;
+    }
+}
+
+internal interface IBusterInstruction
+{
+    string Type { get; }
+}
+
+class ReleaseInstruction : IBusterInstruction
+{
+    public string Type => "RELEASE";
+    public override string ToString()
+    {
+        return Type;
+    }
+}
+
+class BustIntruction : IBusterInstruction
+{
+    public string Type => "BUST";
+
+    public override string ToString()
+    {
+        return Type;
+    }
+}
+
+class MoveInstruction : IBusterInstruction
+{
+    public int X { get; }
+    public int Y { get; }
+    public string Type => "MOVE";
+
+    public MoveInstruction(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString()
+    {
+        return $"{Type} {X} {Y}";
     }
 }
 
